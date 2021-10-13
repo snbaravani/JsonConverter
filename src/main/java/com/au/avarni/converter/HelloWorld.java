d2bc35c189c9c91b8d019a7aa34d7d01c4a266f6
Subbu Baravani <subbu.baravani@iag.com.au>
2021-10-13 18:28:56 +1100

    Draft 1

---
+++ src/main/java/com/au/avarni/converter/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.au.avarni.converter;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+    }
+}
---
+++ src/main/java/com/au/avarni/tabular/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.au.avarni.tabular;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+    }
+}
